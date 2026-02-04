bf4d580b40fb2a9e82aa8aed0acf3c4f9153f757
anushatanugula08 <Tanugula.Anusha@cognizant.com>
2026-02-04 10:13:53 +0530

    folder added

--- Vscode/script/script2.js
+++
@@ -1,8 +0,0 @@
-var fname=prompt("enter first name")
-var lname=prompt("enter last name")
-var name=fname+lname
-alert("your full name is "+name)
-var n1=prompt("enter 1st number")
-var n2=prompt("enter 2nd number")
-var n3=n1*n2
-document.writeln("result is ",n3)
